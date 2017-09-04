ce49f396c31120ce363a5b065531f8ecbf7ee5e6
Youssef Bentaher <ybentaher@gmail.com>
2017-09-04 21:12:53 +0200

    iets in de editor gedaan

--- app/src/main/java/com/example/mocro/firstapp/MainActivity.java
+++ app/src/main/java/com/example/mocro/firstapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.mocro.firstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //een commenttje erbij
+    }
+
+    public void sendMessage(View view){
+        //iets doen!
+        Intent intent =  new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
+
